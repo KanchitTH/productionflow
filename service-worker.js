d9ada198653305193375dc8b63d1cc88ec0bb4fb
KanchitTH <tohniti@gmail.com>
2025-07-17 10:22:31 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
-});
-
-self.addEventListener("fetch", event => {
-  // สำหรับ cache offline เราไม่ได้ใช้งานเพราะโหลดจาก Google Apps Script โดยตรง
-});
